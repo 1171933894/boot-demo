f5f9a9f034a7ec1f4f634c4106d63bcf61c347e9
heyuanxin3 <heyuanxin3@jd.com>
2020-07-03 18:39:26 +0800

    c

--- src/main/java/com/boot/demo/DemoApplication.java
+++ src/main/java/com/boot/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.boot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication是一个组合注解：该注解用来说明被标注的类为SpringBoot的主配置类，如果想要启动该SpringBoot应用就需要运行这个类的main方法，这个方法就是主入口。
+ */
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/boot/demo/controller/HelloController.java
+++ src/main/java/com/boot/demo/controller/HelloController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    //@RequestMapping:标注该方法将处理浏览的的hello请求
+    //@ResponseBody:将controller的方法返回的对象通过适当的转换器转换为指定的格式之后，
+    //写入到response对象的body区，通常用来返回JSON数据或者是XML数据
     @RequestMapping("/")
     @ResponseBody
     public String getHello() {
